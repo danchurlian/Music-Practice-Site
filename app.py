41e316e96cceeb2173497c5a3859528dee921ac5
dchu400 <chudaniel400@gmail.com>
2026-01-20 22:04:07 -0500

    Add basic major scale logic on scale page

--- app.py
+++ app.py
@@ -8,6 +8,41 @@ tk.setResourcePath(os.path.join(os.path.dirname(verovio.__file__), "data"))
 tk.setOptions({
     "inputFrom": "xml",
 })
+
+
+def get_scale_xml(letter: str, mode: str) -> str:
+    result: str = ""
+    base: int = 65
+    start_num: int = ord(letter) - base
+    curr_octave: int = 4
+
+    # Major WWHWWWH OR 2212221, as length 7, do nothing at i == 0
+    # possible each loop num half steps from previous note, then sharp/flatten if needed
+    # creaitng a scale from the step model, more reliable
+
+    # list of all possible scales and their note-octave pairs, hard-coding way no good
+
+    for i in range(8):
+        # i is the scale-degree with 0-based indexing
+        curr_letter_ascii = base + ((start_num + i) % 7) # formula
+        curr_letter: str = chr(curr_letter_ascii)
+        if (i > 0 and curr_letter == "C"):
+            curr_octave += 1 # go up a higher octave if needed
+
+        note_xml: str = f"""
+<note>
+    <pitch>
+        <step>{curr_letter}</step>
+        <octave>{curr_octave}</octave>
+    </pitch>
+    <duration>1</duration>
+    <type>quarter</type>
+</note>
+"""
+        result += note_xml
+    return result
+
+
 def music_xml() -> str:
     return f"""
 <?xml version="1.0" encoding="UTF-8"?>
@@ -36,15 +71,7 @@ def music_xml() -> str:
                     <line>2</line>
                 </clef>
             </attributes>
-            <note>
-                <pitch>
-                    <step>C</step>
-                    <octave>4</octave>
-                </pitch>
-                <duration>1</duration>
-                <type>quarter</type>
-                <stem>up</stem>
-            </note>
+            <NOTES />
         </measure>
     </part>
 </score-partwise>
@@ -53,10 +80,10 @@ def music_xml() -> str:
 @app.route("/scales")
 def scale_page():
     xml: str = music_xml()
+    xml = xml.replace("<NOTES />", get_scale_xml("C", "major"))
     print(xml)
     tk.loadData(xml)
     music_svg: str = tk.renderToSVG(1)
-    print(music_svg)
     return render_template("scale_page.html", music_svg=music_svg)
 
 @app.route("/")
