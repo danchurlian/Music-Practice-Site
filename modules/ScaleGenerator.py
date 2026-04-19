f1e3281717ebac9e5e54a09a1860d86a377fc6e5
dchu400 <chudaniel400@gmail.com>
2026-04-18 23:12:39 -0400

    style: fix indentation of ScaleGenerator.py

--- modules/ScaleGenerator.py
+++ modules/ScaleGenerator.py
@@ -34,103 +34,103 @@ def format_scale_name(letter: str, mode: str, accidental: str = None) -> str:
 
 
 def get_random_scale_info() -> tuple:
-        # random letter and scale mode
-        random_scale_letter: str = chr(random.randint(65, 71)) 
-        random_accidental: str = random.choice(["sharp", "flat", None])
+    # random letter and scale mode
+    random_scale_letter: str = chr(random.randint(65, 71)) 
+    random_accidental: str = random.choice(["sharp", "flat", None])
+    key: str = (random_scale_letter, random_accidental)
+    black_listed_notes: list = [("B", "sharp"), ("E", "sharp"), ("F", "flat")]
+
+    # make sure to NOT choose 
+    while (key in black_listed_notes):
+        random_scale_letter = chr(random.randint(65, 71)) 
+        random_accidental = random.choice(["sharp", "flat", None])
         key: str = (random_scale_letter, random_accidental)
-        black_listed_notes: list = [("B", "sharp"), ("E", "sharp"), ("F", "flat")]
 
-        # make sure to NOT choose 
-        while (key in black_listed_notes):
-            random_scale_letter = chr(random.randint(65, 71)) 
-            random_accidental = random.choice(["sharp", "flat", None])
-            key: str = (random_scale_letter, random_accidental)
+    mode_map = {
+        ("C", "flat"): ["major", "lydian"],
+        ("C", "sharp"): ["major", "dorian", "phrygian", "mixolydian", "locrian", "minor"],
+        ("D", "flat"): ["major", "dorian", "lydian", "mixolydian"],
+        ("D", "sharp"): ["dorian", "phrygian", "locrian", "minor"],
+        ("E", "flat"): ["major", "dorian", "phrygian", "lydian", "mixolydian", "minor"],
+        ("F", "sharp"): ["major", "dorian", "phrygian", "lydian", "mixolydian", "locrian", "minor"],
+        ("G", "flat"): ["major", "lydian", "mixolydian"],
+        ("G", "sharp"): ["dorian", "phrygian", "mixolydian", "locrian", "minor"],
+        ("A", "flat"): ["major", "dorian", "lydian", "mixolydian", "minor"],
+        ("A", "sharp"): ["phrygian", "locrian", "minor"],
+        ("B", "flat"): ["major", "dorian", "phrygian", "lydian", "mixolydian", "locrian", "minor"]
+    }
+    mode_list: list = mode_map[key] if key in mode_map else ["major", "dorian", "phrygian", "lydian", "mixolydian", "locrian", "minor"]
 
-        mode_map = {
-            ("C", "flat"): ["major", "lydian"],
-            ("C", "sharp"): ["major", "dorian", "phrygian", "mixolydian", "locrian", "minor"],
-            ("D", "flat"): ["major", "dorian", "lydian", "mixolydian"],
-            ("D", "sharp"): ["dorian", "phrygian", "locrian", "minor"],
-            ("E", "flat"): ["major", "dorian", "phrygian", "lydian", "mixolydian", "minor"],
-            ("F", "sharp"): ["major", "dorian", "phrygian", "lydian", "mixolydian", "locrian", "minor"],
-            ("G", "flat"): ["major", "lydian", "mixolydian"],
-            ("G", "sharp"): ["dorian", "phrygian", "mixolydian", "locrian", "minor"],
-            ("A", "flat"): ["major", "dorian", "lydian", "mixolydian", "minor"],
-            ("A", "sharp"): ["phrygian", "locrian", "minor"],
-            ("B", "flat"): ["major", "dorian", "phrygian", "lydian", "mixolydian", "locrian", "minor"]
-        }
-        mode_list: list = mode_map[key] if key in mode_map else ["major", "dorian", "phrygian", "lydian", "mixolydian", "locrian", "minor"]
+    # add other minor modes if possible
+    if mode_list[-1] == "minor":
+        mode_list = ["natural minor", "harmonic minor", "melodic minor"]
 
-        # add other minor modes if possible
-        if mode_list[-1] == "minor":
-            mode_list = ["natural minor", "harmonic minor", "melodic minor"]
+    random_mode: str = random.choice(mode_list)
 
-        random_mode: str = random.choice(mode_list)
-
-        return (random_scale_letter, random_mode, random_accidental)
+    return (random_scale_letter, random_mode, random_accidental)
 
 
 def get_xml(letter: str, scale_mode: str, accidental: str = None) -> str:
-        assert scale_mode in SCALE_MAP, f"Invalid scale_mode {scale_mode}"
+    assert scale_mode in SCALE_MAP, f"Invalid scale_mode {scale_mode}"
+    
+    SCALE_STEPS: list = SCALE_MAP[scale_mode]
+    BASE_ASCII: int = 65
+    TONIC_ASCII: int = ord(letter) - BASE_ASCII
+
+    curr_letter: str = letter
+    curr_octave: int = 4
+    bonus: int = (1 if (accidental == "sharp")
+        else -1 if (accidental == "flat")
+        else 0)
+    result: str = ""
+
+
+    for i in range(8):
+        # i is the scale-degree with 0-based indexing
+        prev_letter: str = curr_letter
+        curr_letter_ascii = BASE_ASCII + ((TONIC_ASCII + i) % 7) # formula
+        curr_letter = chr(curr_letter_ascii)
+
+        # increment the octave when the current letter is "C"
+        if (i > 0 and curr_letter == "C"):
+            curr_octave += 1
         
-        SCALE_STEPS: list = SCALE_MAP[scale_mode]
-        BASE_ASCII: int = 65
-        TONIC_ASCII: int = ord(letter) - BASE_ASCII
+        # Adjust accidentals if needed
+        curr_accidental: str = None
+        if (i > 0):
+            step: int = SCALE_STEPS[i-1] # step structure for the scale
+            letter_dist: int = NoteInfoHandler.get_half_step_adjacent_notes(curr_letter, prev_letter) - bonus
 
-        curr_letter: str = letter
-        curr_octave: int = 4
-        bonus: int = (1 if (accidental == "sharp")
-            else -1 if (accidental == "flat")
-            else 0)
-        result: str = ""
-
-
-        for i in range(8):
-            # i is the scale-degree with 0-based indexing
-            prev_letter: str = curr_letter
-            curr_letter_ascii = BASE_ASCII + ((TONIC_ASCII + i) % 7) # formula
-            curr_letter = chr(curr_letter_ascii)
-
-            # increment the octave when the current letter is "C"
-            if (i > 0 and curr_letter == "C"):
-                curr_octave += 1
-            
-            # Adjust accidentals if needed
-            curr_accidental: str = None
-            if (i > 0):
-                step: int = SCALE_STEPS[i-1] # step structure for the scale
-                letter_dist: int = NoteInfoHandler.get_half_step_adjacent_notes(curr_letter, prev_letter) - bonus
-
-                if (letter_dist - step == 1):
-                    curr_accidental = "flat"
-                    bonus = -1
-                elif (letter_dist - step == 2):
-                    curr_accidental = "flat-flat"
-                    bonus = -2
-                elif (letter_dist - step == -2):
-                    curr_accidental = "double-sharp"
-                    bonus = 2
-                elif (letter_dist - step == -1):
-                    curr_accidental = "sharp"
-                    bonus = 1
-                else:
-                    bonus = 0
+            if (letter_dist - step == 1):
+                curr_accidental = "flat"
+                bonus = -1
+            elif (letter_dist - step == 2):
+                curr_accidental = "flat-flat"
+                bonus = -2
+            elif (letter_dist - step == -2):
+                curr_accidental = "double-sharp"
+                bonus = 2
+            elif (letter_dist - step == -1):
+                curr_accidental = "sharp"
+                bonus = 1
             else:
-                # sharpen or flatten the first note, the tonic (C# major / minor for instance)
-                if (bonus == 1):
-                    curr_accidental = "sharp"
-                elif (bonus == -1):
-                    curr_accidental = "flat"
+                bonus = 0
+        else:
+            # sharpen or flatten the first note, the tonic (C# major / minor for instance)
+            if (bonus == 1):
+                curr_accidental = "sharp"
+            elif (bonus == -1):
+                curr_accidental = "flat"
 
-            note = NoteBuilder() \
-                .set_step(curr_letter) \
-                .set_octave(curr_octave) \
-                .set_accidental(curr_accidental) \
-                .build()
-            note_xml: str = note.get_xml()
-            result += note_xml
+        note = NoteBuilder() \
+            .set_step(curr_letter) \
+            .set_octave(curr_octave) \
+            .set_accidental(curr_accidental) \
+            .build()
+        note_xml: str = note.get_xml()
+        result += note_xml
 
-        return result
+    return result
 
 class ScaleInfo(object):
     def __init__(self, start_letter: str,
